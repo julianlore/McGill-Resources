6ea76c4114642647504291ad7a3214960662c7bc
Julian Lore <julian.lorej@gmail.com>
2017-04-24 23:46:27 -0400

    My answers for C206 F2013, need to fix Q4 and Q5

---
+++ COMP206/Practice/F2013/Q5.py
@@ -0,0 +1,46 @@
+import cgi
+
+input = cgi.FieldStorage() # Get input
+
+# Get the fields we want
+filename = input["filename"]
+lines = int(input["lines"]) # Cast to int
+
+# HTML output
+print("Content-type: text/html\n\n")
+print("<html>")
+print("<header><title>Products</title></header>")
+print("<body>")
+# Try opening CSV file
+try:
+    file = fopen(filename,"r")
+except: # Error
+    print("File not found")
+else:
+    print("<table>")
+    print("<tr>")
+    print("<th> Product Code </th>")
+    print("<th> Quantity in Stock</th")
+    print("<th> Min in Stock</th>")
+    print("<th>Unit Price</th>")
+    print("<th> Min to Purchase</th>")
+    for i in range(lines): # Loop through lines
+        data = file.readline()
+        # Tokenize?
+        code = ""
+        quantity = ""
+        min = ""
+        unit = ""
+        min2 = min-quantity
+        if quantity<min:
+            print("<tr>")
+            print("<td> %d </td>"%(data))
+            print("<td> %d </td>"%(quantity))
+            print("<td> %d </td>"%(min))
+            print("<td> %d </td>"%(unit))
+            print("<td> %d </td>"%(min2))
+            print("</tr>")
+
+    print("</table>")
+print("</body>")
+print("</html>")
