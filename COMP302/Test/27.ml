606a0560a7c38c0d4bca185c6f8a27c0f56b6d91
Julian Lore <julian.lore@mail.mcgill.ca>
2017-11-14 13:16:16 -0500

    Added COMP251 Lecture 18 and COMP302 Lecture 27
    Former-commit-id: ad8790f8106db620f7941e269cca6f569698c250
    Former-commit-id: 7d0ec60da6b419592a18af654c9ad0a269cbc73a

---
+++ COMP302/Test/27.ml
@@ -0,0 +1,42 @@
+(* Defining a new language in OCaml *)
+type primop = Equals | LessThan | Plus | Minus | Times | Negate
+
+type exp = 
+  | Int of int (* 0 | 1 | 2 | ... *)
+  | Bool of bool (* true | false *)
+  | If of exp * exp * exp (* if e then e_1 else e_2 *)
+  | Primop of primop * exp list (*e_1 primop e_2 or primop e *)
+
+(* How to evaluate? *)
+module Eval =
+  struct
+    open exp
+
+    exception Stuck of string
+
+    let evalOp op = match op with
+      | (Equals, [Int i; Int i']) -> Some (Bool (i = i'))
+      | (LessThan, [Int i; Int i']) -> Some (Bool (i < i'))
+      | (Plus, [Int i; Int i']) -> Some (Int (i + i'))
+      | (Minus, [Int i; Int i']) -> Some (Int (i - i'))
+      | (Times, [Int i; Int i']) -> Some (Int (i * i'))
+      | (Negate, [Int i]) -> Some (Int (-i))
+      | _ -> None
+           
+    let rec eval e = match e with
+      | Int _ -> e
+      | Bool _ -> e
+      | If (e, e1, e2) ->
+         (match eval e with
+          | Bool true -> eval e1
+          | Bool false -> eval e2
+          | _ -> raise (Stuck "guard is not  bool"))
+           (* Primitive operations *)
+      | Primop (po, args) ->
+         let argvalues = List.map eval args in
+         (match evalOp (po, argvalues) with
+          | Some v -> v
+          | None -> raise (Stuck "wrong arguments passed to prim. op"))
+    
+  end
+    (* The rest is on course page *)
